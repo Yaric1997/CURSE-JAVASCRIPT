e31dbfd5e8b1051b373842326db8078cf7d4ec00
Yaroslav <yaroslav.lukash@ukr.net>
2020-06-16 22:08:47 +0300

    add object personalMovieDB with properties
    count, movies, actors, genres, privat

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 "use strict";
 const numberOfFilms = +prompt('Сколько фильмов ви уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
