f30f64bf914def3064247fdf01dcb2fbb40be186
Yaroslav <yaroslav.lukash@ukr.net>
2020-06-18 00:57:39 +0300

    add str.length < 50 don't empty str and btn cancel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 const numberOfFilms = +prompt('Сколько фильмов ви уже посмотрели?', '');
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +8,70 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один ин просмотрених фильмов?', ''),
-  b = prompt('На сколько оцените его?', ''),
-  c = prompt('Один ин просмотрених фильмов?', ''),
-  d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+/*for (let i = 0; i < 2; i++){
+  const a = prompt('Один ин просмотрених фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB);
+  if (a != null && b != null && a !== '' && b !== '' && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  if (personalMovieDB.count < 10 ){
+    console.log('Watch little films');
+  }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You classic watcher');
+  }else if (personalMovieDB.count >= 30){
+    console.log('You kinofan');
+  }else {
+    console.log('Error');
+  }
+}
+
+while (let  i < 2; i++){
+  const a = prompt('Один ин просмотрених фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+  if (a != null && b != null && a !== '' && b !== '' && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  if (personalMovieDB.count < 10 ){
+    console.log('Watch little films');
+  }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You classic watcher');
+  }else if (personalMovieDB.count >= 30){
+    console.log('You kinofan');
+  }else {
+    console.log('Error');
+  }
+}*/
+let  i;
+do {
+  const a = prompt('Один ин просмотрених фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+  if (a != null && b != null && a !== '' && b !== '' && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  if (personalMovieDB.count < 10 ){
+    console.log('Watch little films');
+  }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You classic watcher');
+  }else if (personalMovieDB.count >= 30){
+    console.log('You kinofan');
+  }else {
+    console.log('Error');
+  }
+  i++;
+}while (i < 2)
